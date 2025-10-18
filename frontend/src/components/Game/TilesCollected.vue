b6d636e82d68475c20f3cf4d6491ad545f4b19cb
blomtga <timothy.blom@hva.nl>
2025-10-18 21:16:09 +0200

    updated workspace and unversioned

---
+++ frontend/src/components/Game/TilesCollected.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="tiles-container tiles-scroll">
+    <div v-for="tile in tilesOnTable" :key="tile.value" class="tile">
+      <div class="tile-value">{{ tile.value }}</div>
+      <div class="tile-worms">🐛 x{{ tile.worms }}</div>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+defineProps({
+  tiles: Array,
+  isCurrentPlayer: Boolean
+});
+</script>
